c4971fb0a85805b292434365054477caba15b907
somosomosomosan <nagekonapps@gmail.com>
2023-07-21 15:51:05 +0900

    pow

---
+++ src/app/categories/smash/page.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function PageSmash() {
+	return (
+		<div>
+			<p>すまぶら</p>
+			<Link to={'/'}>ホーム</Link>
+		</div>
+	);
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import PageSmash from '../categories/smash/page';
+import logo from '../../logo.svg';
+import '../../App.css';
+
+export default function PageHome() {
+	console.log({ PUBLIC_URL: process.env.PUBLIC_URL });
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Content />} />
+				<Route path='/smash' element={<PageSmash />} />
+			</Routes>
+		</div>
+	);
+}
+function Content() {
+	return (
+		<div>
+			<Link to={'/smash'}>スマブラ</Link>
+			<img src={logo} className='App-logo' alt='logo' />
+		</div>
+	);
+}
