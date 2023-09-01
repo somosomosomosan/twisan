5e696eebcf835756b9886f1827885b3f56260a6b
somosomosomosan <nagekonapps@gmail.com>
2023-09-01 10:46:03 +0900

    twisan

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-//ローカルなら無（''） github pagesスラッシュから始まる 例：/twt
+//ローカルなら無（''） github pagesスラッシュから始まる 例：/twisan
 const REPO_NAME = process.env.PUBLIC_URL;
 export const PATH_RANKING_DATA = `${REPO_NAME}/data`;
 export const PATH_ASSETS = `${REPO_NAME}/assets`;
